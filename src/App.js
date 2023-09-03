0596d1b7729b5ff954a2131cea165962a019b041
akshay <akshayexazad@gmail.com>
2023-09-03 21:50:20 +0530

    nike-clone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Ecounter from "./Ecounter";
-
-function App(){
- return(
- <>
-  <Ecounter/>
- </>
- );
+import './style.css';
+import Navigation from './Components/Navigation';
+import Hero from './Components/Hero';
+const App = ()=>{
+    return (
+        <div>
+           <Navigation/>
+           <Hero/>
+        </div>
+    );
 };
 
 
